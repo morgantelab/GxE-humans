1ceca5e214d01c52e050b3b6f852db215f9f7401
fmorgante <fabiom@clemson.edu>
2024-06-04 16:09:01 -0400

    Minor changes

--- cleaned_scripts/6_run.R
+++ cleaned_scripts/6_run.R
@@ -10,16 +10,19 @@ NAcol <- function(X){for(i in 1:ncol(X)){print(length(which(is.na(X[i]))))}}
 censcale <- function(x,A,B){(A+(((x-mean(x)))/sd(x))*B)}
 
 
-
+args <- as.numeric(commandArgs(TRUE))
 
 
 iter <- 60000
 burnin <- 10000
 thin <- 50
 verb <- T
-where <- paste("run",args[8],sep=''); where
 home <- ""
 scratch <- ""
+trait <- args[1] #DP,SP,PP
+model <- args[2] #M0,M1, etc.
+analysis <- args[3] #Whole data, Y-O, RND
+where <- paste("run",args[4],sep=''); where
 
 
 
@@ -59,7 +62,7 @@ nrow(dt)
 
 Ivars <- c("ID", "Sex_SI", "Sex_gen", "YOB", "MOB", "DOF", "COF", "ethn1", "ethn1_white", "ethn1_mixed", "ethn1_asian", "ethn1_black", "ethn2", "AOP", "AOR")
 Gvars <- Val$pc; length(Gvars)
-Emarc <-	c("Townsend", "walk_d", "act0_d", "act1_d", "TVtime", "sleep_d", "smoking_now", "veg_cook", "fish_oily", "fish_lean", "meat_proc", "poultry", "beef", "lamb", "pork", "cheese", "salt", "tea", "alc1", "waist")
+Emarc <- c("Townsend", "walk_d", "act0_d", "act1_d", "TVtime", "sleep_d", "smoking_now", "veg_cook", "fish_oily", "fish_lean", "meat_proc", "poultry", "beef", "lamb", "pork", "cheese", "salt", "tea", "alc1", "waist")
 Etiez <- c("PCtime", "DRtime", "getup", "coffee", "smoked_past", "BFP", "BMR")
 Evars <- c(Emarc, Etiez); length(Evars)
 Yvars <- c("PR0", "PR1",   "DP0", "DP1",   "SP0", "SP1",   "PP0", "PP1",   "PPm",    "DP0a", "DP1a",   "SP0a", "SP1a",  "PP0a", "PP1a",   "PPam",    "BMI1", "BMI2") 
@@ -79,12 +82,6 @@ Ymat <- as.matrix(dt[Yvars]); dim(Ymat); NAcol(Ymat)
 Xmat <- as.matrix(dt[cohorts]); dim(Xmat); NAcol(Xmat)
 
 Gmat <- as.matrix(dt[Gvars]); dim(Gmat); # NAcol(Gmat)
-
-
-
-
-
-if(model%in%c("m0"){}
 
 
 
@@ -174,20 +171,20 @@ if(model%in%c("m23")){
 
 dt$trn <- NA
 
-if(args[3]==1){dt$trn <- 1}
+if(analysis==1){dt$trn <- 1}
 
-if(args[3]==11){dt$trn <- replace(dt$trn, dt$sub%in%c(11,12,13,14), 1)}
+if(analysis==11){dt$trn <- replace(dt$trn, dt$sub%in%c(11,12,13,14), 1)}
 
-if(args[3]==103){dt$trn <- replace(dt$trn, dt$coh%in%c("0_1", "0_2", "1_1", "1_2"), 1)} # C
+if(analysis==103){dt$trn <- replace(dt$trn, dt$coh%in%c("0_1", "0_2", "1_1", "1_2"), 1)} # C
 
 summary(dt$trn)
 nrow(dt)
 
-if(args[3]==1){Xmat <- Xmat[,-1]}
-if(args[3]==101){Xmat <- Xmat[,which(colnames(Xmat)%in%c("coh01", "coh02", "coh03", "coh04"))]; Xmat <- Xmat[,-1]} 
-if(args[3]==102){Xmat <- Xmat[,which(colnames(Xmat)%in%c("coh11", "coh12", "coh13", "coh14"))]; Xmat <- Xmat[,-1]} 
-if(args[3]==103){Xmat <- Xmat[,which(colnames(Xmat)%in%c("coh01", "coh02", "coh11", "coh12"))]; Xmat <- Xmat[,-1]} 
-if(args[3]==104){Xmat <- Xmat[,which(colnames(Xmat)%in%c("coh03", "coh04", "coh13", "coh14"))]; Xmat <- Xmat[,-1]} 
+if(analysis==1){Xmat <- Xmat[,-1]}
+if(analysis==101){Xmat <- Xmat[,which(colnames(Xmat)%in%c("coh01", "coh02", "coh03", "coh04"))]; Xmat <- Xmat[,-1]} 
+if(analysis==102){Xmat <- Xmat[,which(colnames(Xmat)%in%c("coh11", "coh12", "coh13", "coh14"))]; Xmat <- Xmat[,-1]} 
+if(analysis==103){Xmat <- Xmat[,which(colnames(Xmat)%in%c("coh01", "coh02", "coh11", "coh12"))]; Xmat <- Xmat[,-1]} 
+if(analysis==104){Xmat <- Xmat[,which(colnames(Xmat)%in%c("coh03", "coh04", "coh13", "coh14"))]; Xmat <- Xmat[,-1]} 
 
 summary(Xmat)
 gc()
@@ -205,12 +202,12 @@ gc()
 ##################################
 
 
-Phen <- phen <- Ymat[,args[1]]
+Phen <- phen <- Ymat[,trait]
 print(mean(phen))
 #rm(Ymat)
 
 
-if(args[3]>1){
+if(analysis>1){
 	phen <- phen*dt$trn
 	print(summary(phen))
 }
@@ -227,7 +224,7 @@ rm(dt); gc()
 
 # Define model #
 
-if(args[2]%in%c("m0")){
+if(model%in%c("m0")){
   
   ETA <- list(X=list(X=Xmat, model="FIXED", saveEffects=TRUE), 
               G=list(X=Gmat, model="BRR", saveEffects=TRUE) 
@@ -237,7 +234,25 @@ if(args[2]%in%c("m0")){
 }
 
 
-if(args[2]%in%c("m1","m2","m3")){
+if(model%in%c("m01")){
+  
+  ETA <- list(X=list(X=Xmat, model="FIXED", saveEffects=TRUE), 
+              G=list(X=Gmat[,1:20], model="BRR", saveEffects=TRUE) 
+  )
+   rm(Xmat, Gmat); gc()
+
+}
+
+
+if(model%in%c("m02")){
+  
+  ETA <- list(X=list(X=Xmat, model="FIXED", saveEffects=TRUE))
+   rm(Xmat); gc()
+
+}
+
+
+if(model%in%c("m1","m2","m3")){
   
   ETA <- list(X=list(X=Xmat, model="FIXED", saveEffects=TRUE), 
               E=list(X=Emat, model="BRR", saveEffects=TRUE)
@@ -247,7 +262,7 @@ if(args[2]%in%c("m1","m2","m3")){
 }
 
 
-if(args[2]%in%c("m11","m12","m13")){
+if(model%in%c("m11","m12","m13")){
   
   ETA <- list(X=list(X=Xmat, model="FIXED", saveEffects=TRUE), 
               G=list(X=Gmat, model="BRR", saveEffects=TRUE), 
@@ -258,7 +273,7 @@ if(args[2]%in%c("m11","m12","m13")){
 }
 
 
-if(args[2]%in%c("m21","m22","m23")){
+if(model%in%c("m21","m22","m23")){
 
   ETA <- list(X=list(X=Xmat, model="FIXED", saveEffects=TRUE), 
               G=list(X=Gmat, model="BRR", saveEffects=TRUE), 
@@ -276,7 +291,7 @@ if(args[2]%in%c("m21","m22","m23")){
 # Run model #
 
 
-model <- BGLR(y=phen, ETA=ETA, nIter=iter, burnIn=burnin, thin=thin, verbose=verb, saveAt=paste(scratch, '/run_', args[1], '_', args[2], '_', args[3], '_', sep=''))
+model <- BGLR(y=phen, ETA=ETA, nIter=iter, burnIn=burnin, thin=thin, verbose=verb, saveAt=paste(scratch, '/run_', trait, '_', model, '_', analysis, '_', sep=''))
 
 
 
@@ -287,16 +302,16 @@ model <- BGLR(y=phen, ETA=ETA, nIter=iter, burnIn=burnin, thin=thin, verbose=ver
 # Collect results #
 
 
-if(model%in%c("m0")){
+if(model%in%c("m0", "m01")){
   
-  zz0 <- read.table(paste(scratch, '/run_', args[1], '_', args[2], '_', args[3], '_mu.dat', sep=''), header=F); colnames(zz0) <- "int"
-  zz1 <- read.table(paste(scratch, '/run_', args[1], '_', args[2], '_', args[3], '_ETA_G_varB.dat', sep=''), header=F); colnames(zz1) <- "G"
+  zz0 <- read.table(paste(scratch, '/run_', trait, '_', model, '_', analysis, '_mu.dat', sep=''), header=F); colnames(zz0) <- "int"
+  zz1 <- read.table(paste(scratch, '/run_', trait, '_', model, '_', analysis, '_ETA_G_varB.dat', sep=''), header=F); colnames(zz1) <- "G"
   zz2 <- data.frame(matrix(0, nrow(zz0), 1)); colnames(zz2) <- "E"
-  zz9 <- read.table(paste(scratch, '/run_', args[1], '_', args[2], '_', args[3], '_varE.dat', sep=''), header=F); colnames(zz9) <- "res"
+  zz9 <- read.table(paste(scratch, '/run_', trait, '_', model, '_', analysis, '_varE.dat', sep=''), header=F); colnames(zz9) <- "res"
   VCEm <- data.frame(zz0, zz1, zz2, zz9)
   
-  B1 <- read.table(paste(scratch, '/run_', args[1], '_', args[2], '_', args[3], '_ETA_X_b.dat', sep=''), header=T)
-  B2 <- readBinMat(paste(scratch, '/run_', args[1], '_', args[2], '_', args[3], '_ETA_G_b.bin', sep=''))
+  B1 <- read.table(paste(scratch, '/run_', trait, '_', model, '_', analysis, '_ETA_X_b.dat', sep=''), header=T)
+  B2 <- readBinMat(paste(scratch, '/run_', trait, '_', model, '_', analysis, '_ETA_G_b.bin', sep=''))
   B3 <- data.frame(matrix(0, nrow(zz0), length(Evars)))
   
   varabs <- matrix(NA, nrow(VCEm), 3); colnames(varabs) <- c("Vdes", "Vgen", "Venv")
@@ -307,10 +322,40 @@ if(model%in%c("m0")){
   setwd(home) 
   
   VCE <- data.frame(VCEm, varabs, B1)
-  save(VCE, file=paste(paste(where, '_results/VCE_', args[1], '_', args[2], '_', args[3], '.RData', sep='')))
+  save(VCE, file=paste(paste(where, '_results/VCE_', trait, '_', model, '_', analysis, '.RData', sep='')))
   
   preds	<- data.frame(preds, y=model$y, yHat=model$yHat)
-  save(preds, file=paste(paste(where, '_results/PREDS_', args[1], '_', args[2], '_', args[3], '.RData', sep='')))
+  save(preds, file=paste(paste(where, '_results/PREDS_', trait, '_', model, '_', analysis, '.RData', sep='')))
+ 
+  rm(ETA, model, zz0, zz1, zz2, zz9, B1, B2, B3, varabs, VCE, VCEm)
+  
+}
+
+
+if(model%in%c("m02")){
+  
+  zz0 <- read.table(paste(scratch, '/run_', trait, '_', model, '_', analysis, '_mu.dat', sep=''), header=F); colnames(zz0) <- "int"
+  zz1 <- data.frame(matrix(0, nrow(zz0), 1)); colnames(zz1) <- "G"
+  zz2 <- data.frame(matrix(0, nrow(zz0), 1)); colnames(zz2) <- "E"
+  zz9 <- read.table(paste(scratch, '/run_', trait, '_', model, '_', analysis, '_varE.dat', sep=''), header=F); colnames(zz9) <- "res"
+  VCEm <- data.frame(zz0, zz1, zz2, zz9)
+  
+  B1 <- read.table(paste(scratch, '/run_', trait, '_', model, '_', analysis, '_ETA_X_b.dat', sep=''), header=T)
+  B2 <- data.frame(matrix(0, nrow(zz0), length(Gvars)))
+  B3 <- data.frame(matrix(0, nrow(zz0), length(Evars)))
+  
+  varabs <- matrix(NA, nrow(VCEm), 3); colnames(varabs) <- c("Vdes", "Vgen", "Venv")
+  varabs[, 1] <- apply(ETA$X$X%*%t(B1), 2, var)
+  varabs[-c(1:(burnin/thin)), 2] <- 0
+  varabs[-c(1:(burnin/thin)), 3] <- 0
+ 
+  setwd(home) 
+  
+  VCE <- data.frame(VCEm, varabs, B1)
+  save(VCE, file=paste(paste(where, '_results/VCE_', trait, '_', model, '_', analysis, '.RData', sep='')))
+  
+  preds	<- data.frame(preds, y=model$y, yHat=model$yHat)
+  save(preds, file=paste(paste(where, '_results/PREDS_', trait, '_', model, '_', analysis, '.RData', sep='')))
  
   rm(ETA, model, zz0, zz1, zz2, zz9, B1, B2, B3, varabs, VCE, VCEm)
   
@@ -319,15 +364,15 @@ if(model%in%c("m0")){
 
 if(model%in%c("m1","m2","m3")){
   
-  zz0 <- read.table(paste(scratch, '/run_', args[1], '_', args[2], '_', args[3], '_mu.dat', sep=''), header=F); colnames(zz0) <- "int"
+  zz0 <- read.table(paste(scratch, '/run_', trait, '_', model, '_', analysis, '_mu.dat', sep=''), header=F); colnames(zz0) <- "int"
   zz1 <- data.frame(matrix(0, nrow(zz0), 1)); colnames(zz1) <- "G"
-  zz2 <- read.table(paste(scratch, '/run_', args[1], '_', args[2], '_', args[3], '_ETA_E_varB.dat', sep=''), header=F); colnames(zz2) <- "E"
-  zz9 <- read.table(paste(scratch, '/run_', args[1], '_', args[2], '_', args[3], '_varE.dat', sep=''), header=F); colnames(zz9) <- "res"
+  zz2 <- read.table(paste(scratch, '/run_', trait, '_', model, '_', analysis, '_ETA_E_varB.dat', sep=''), header=F); colnames(zz2) <- "E"
+  zz9 <- read.table(paste(scratch, '/run_', trait, '_', model, '_', analysis, '_varE.dat', sep=''), header=F); colnames(zz9) <- "res"
   VCEm <- data.frame(zz0, zz1, zz2, zz9)
   
-  B1 <- read.table(paste(scratch, '/run_', args[1], '_', args[2], '_', args[3], '_ETA_X_b.dat', sep=''), header=T)
+  B1 <- read.table(paste(scratch, '/run_', trait, '_', model, '_', analysis, '_ETA_X_b.dat', sep=''), header=T)
   B2 <- data.frame(matrix(0, nrow(zz0), length(Gvars)))
-  B3 <- readBinMat(paste(scratch, '/run_', args[1], '_', args[2], '_', args[3], '_ETA_E_b.bin', sep=''))
+  B3 <- readBinMat(paste(scratch, '/run_', trait, '_', model, '_', analysis, '_ETA_E_b.bin', sep=''))
   
   varabs <- matrix(NA, nrow(VCEm), 3); colnames(varabs) <- c("Vdes", "Vgen", "Venv")
   varabs[, 1] <- apply(ETA$X$X%*%t(B1), 2, var)
@@ -337,10 +382,10 @@ if(model%in%c("m1","m2","m3")){
   setwd(home) 
   
   VCE <- data.frame(VCEm, varabs, B1)
-  save(VCE, file=paste(paste(where, '_results/VCE_', args[1], '_', args[2], '_', args[3], '.RData', sep='')))
+  save(VCE, file=paste(paste(where, '_results/VCE_', trait, '_', model, '_', analysis, '.RData', sep='')))
   
   preds	<- data.frame(preds, y=model$y, yHat=model$yHat)
-  save(preds, file=paste(paste(where, '_results/PREDS_', args[1], '_', args[2], '_', args[3], '.RData', sep='')))
+  save(preds, file=paste(paste(where, '_results/PREDS_', trait, '_', model, '_', analysis, '.RData', sep='')))
  
   rm(ETA, model, zz0, zz1, zz2, zz9, B1, B2, B3, varabs, VCE, VCEm)
   
@@ -349,15 +394,15 @@ if(model%in%c("m1","m2","m3")){
 
 if(model%in%c("m11","m12","m13")){
   
-  zz0 <- read.table(paste(scratch, '/run_', args[1], '_', args[2], '_', args[3], '_mu.dat', sep=''), header=F); colnames(zz0) <- "int"
-  zz1 <- read.table(paste(scratch, '/run_', args[1], '_', args[2], '_', args[3], '_ETA_G_varB.dat', sep=''), header=F); colnames(zz1) <- "G"
-  zz2 <- read.table(paste(scratch, '/run_', args[1], '_', args[2], '_', args[3], '_ETA_E_varB.dat', sep=''), header=F); colnames(zz2) <- "E"
-  zz9 <- read.table(paste(scratch, '/run_', args[1], '_', args[2], '_', args[3], '_varE.dat', sep=''), header=F); colnames(zz9) <- "res"
+  zz0 <- read.table(paste(scratch, '/run_', trait, '_', model, '_', analysis, '_mu.dat', sep=''), header=F); colnames(zz0) <- "int"
+  zz1 <- read.table(paste(scratch, '/run_', trait, '_', model, '_', analysis, '_ETA_G_varB.dat', sep=''), header=F); colnames(zz1) <- "G"
+  zz2 <- read.table(paste(scratch, '/run_', trait, '_', model, '_', analysis, '_ETA_E_varB.dat', sep=''), header=F); colnames(zz2) <- "E"
+  zz9 <- read.table(paste(scratch, '/run_', trait, '_', model, '_', analysis, '_varE.dat', sep=''), header=F); colnames(zz9) <- "res"
   VCEm <- data.frame(zz0, zz1, zz2, zz9)
   
-  B1 <- read.table(paste(scratch, '/run_', args[1], '_', args[2], '_', args[3], '_ETA_X_b.dat', sep=''), header=T)
-  B2 <- readBinMat(paste(scratch, '/run_', args[1], '_', args[2], '_', args[3], '_ETA_G_b.bin', sep=''))
-  B3 <- readBinMat(paste(scratch, '/run_', args[1], '_', args[2], '_', args[3], '_ETA_E_b.bin', sep=''))
+  B1 <- read.table(paste(scratch, '/run_', trait, '_', model, '_', analysis, '_ETA_X_b.dat', sep=''), header=T)
+  B2 <- readBinMat(paste(scratch, '/run_', trait, '_', model, '_', analysis, '_ETA_G_b.bin', sep=''))
+  B3 <- readBinMat(paste(scratch, '/run_', trait, '_', model, '_', analysis, '_ETA_E_b.bin', sep=''))
   
   varabs <- matrix(NA, nrow(VCEm), 3); colnames(varabs) <- c("Vdes", "Vgen", "Venv")
   varabs[, 1] <- apply(ETA$X$X%*%t(B1), 2, var)
@@ -367,10 +412,10 @@ if(model%in%c("m11","m12","m13")){
   setwd(home) 
   
   VCE <- data.frame(VCEm, varabs, B1)
-  save(VCE, file=paste(paste(where, '_results/VCE_', args[1], '_', args[2], '_', args[3], '.RData', sep='')))
+  save(VCE, file=paste(paste(where, '_results/VCE_', trait, '_', model, '_', analysis, '.RData', sep='')))
   
   preds	<- data.frame(preds, y=model$y, yHat=model$yHat)
-  save(preds, file=paste(paste(where, '_results/PREDS_', args[1], '_', args[2], '_', args[3], '.RData', sep='')))
+  save(preds, file=paste(paste(where, '_results/PREDS_', trait, '_', model, '_', analysis, '.RData', sep='')))
  
   rm(ETA, model, zz0, zz1, zz2, zz9, B1, B2, B3, varabs, VCE, VCEm)
   
@@ -379,17 +424,17 @@ if(model%in%c("m11","m12","m13")){
 
 if(model%in%c("m21","m22","m23")){
   
-  zz0 <- read.table(paste(scratch, '/run_', args[1], '_', args[2], '_', args[3], '_mu.dat', sep=''), header=F); colnames(zz0) <- "int"
-  zz1 <- read.table(paste(scratch, '/run_', args[1], '_', args[2], '_', args[3], '_ETA_G_varB.dat', sep=''), header=F); colnames(zz1) <- "G"
-  zz2 <- read.table(paste(scratch, '/run_', args[1], '_', args[2], '_', args[3], '_ETA_E_varB.dat', sep=''), header=F); colnames(zz2) <- "E"
-  zz3 <- read.table(paste(scratch, '/run_', args[1], '_', args[2], '_', args[3], '_ETA_GE_varB.dat', sep=''), header=F); colnames(zz3) <- "GE"
-  zz9 <- read.table(paste(scratch, '/run_', args[1], '_', args[2], '_', args[3], '_varE.dat', sep=''), header=F); colnames(zz9) <- "res"
+  zz0 <- read.table(paste(scratch, '/run_', trait, '_', model, '_', analysis, '_mu.dat', sep=''), header=F); colnames(zz0) <- "int"
+  zz1 <- read.table(paste(scratch, '/run_', trait, '_', model, '_', analysis, '_ETA_G_varB.dat', sep=''), header=F); colnames(zz1) <- "G"
+  zz2 <- read.table(paste(scratch, '/run_', trait, '_', model, '_', analysis, '_ETA_E_varB.dat', sep=''), header=F); colnames(zz2) <- "E"
+  zz3 <- read.table(paste(scratch, '/run_', trait, '_', model, '_', analysis, '_ETA_GE_varB.dat', sep=''), header=F); colnames(zz3) <- "GE"
+  zz9 <- read.table(paste(scratch, '/run_', trait, '_', model, '_', analysis, '_varE.dat', sep=''), header=F); colnames(zz9) <- "res"
   VCEm <- data.frame(zz0, zz1, zz2, zz3, zz9)
   
-  B1 <- read.table(paste(scratch, '/run_', args[1], '_', args[2], '_', args[3], '_ETA_X_b.dat', sep=''), header=T)
-  B2 <- readBinMat(paste(scratch, '/run_', args[1], '_', args[2], '_', args[3], '_ETA_G_b.bin', sep=''))
-  B3 <- readBinMat(paste(scratch, '/run_', args[1], '_', args[2], '_', args[3], '_ETA_E_b.bin', sep=''))
-  B4 <- readBinMat(paste(scratch, '/run_', args[1], '_', args[2], '_', args[3], '_ETA_GE_b.bin', sep=''))
+  B1 <- read.table(paste(scratch, '/run_', trait, '_', model, '_', analysis, '_ETA_X_b.dat', sep=''), header=T)
+  B2 <- readBinMat(paste(scratch, '/run_', trait, '_', model, '_', analysis, '_ETA_G_b.bin', sep=''))
+  B3 <- readBinMat(paste(scratch, '/run_', trait, '_', model, '_', analysis, '_ETA_E_b.bin', sep=''))
+  B4 <- readBinMat(paste(scratch, '/run_', trait, '_', model, '_', analysis, '_ETA_GE_b.bin', sep=''))
   
   varabs <- matrix(NA, nrow(VCEm), 4); colnames(varabs) <- c("Vdes", "Vgen", "Venv", "Vgenenv")
   varabs[, 1] <- apply(ETA$X$X%*%t(B1), 2, var)
@@ -400,10 +445,10 @@ if(model%in%c("m21","m22","m23")){
   setwd(home) 
   
   VCE <- data.frame(VCEm, varabs, B1)
-  save(VCE, file=paste(paste(where, '_results/VCE_', args[1], '_', args[2], '_', args[3], '.RData', sep='')))
+  save(VCE, file=paste(paste(where, '_results/VCE_', trait, '_', model, '_', analysis, '.RData', sep='')))
   
   preds	<- data.frame(preds, y=model$y, yHat=model$yHat)
-  save(preds, file=paste(paste(where, '_results/PREDS_', args[1], '_', args[2], '_', args[3], '.RData', sep='')))
+  save(preds, file=paste(paste(where, '_results/PREDS_', trait, '_', model, '_', analysis, '.RData', sep='')))
  
   rm(ETA, model, zz0, zz1, zz2, zz3, zz9, B1, B2, B3, B4, varabs, VCE, VCEm)
   
